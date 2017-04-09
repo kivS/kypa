58acc32efdb2381bb091542ec5a6bad5ec9afce6
Vik Borges <v1k@protonmail.com>
2017-04-09 03:18:11 +0100

    project start up

---
+++ main.js
@@ -0,0 +1,18 @@
+const {app} = require('electron');
+
+
+/**
+ *  App Events
+ */
+app.on('ready', start);
+
+
+
+
+
+/**
+ *  Program start Up
+ */
+function start() {
+  console.log("hello biatch!");
+}
