a823b4ae53e1a6d10e83609f3ca2079ee426c09b
Vik Borges <v1k@protonmail.com>
2017-04-27 22:33:37 +0100

    added initial markup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Icon, Button, Grid, Container, Input, Divider, TextArea, Form, Table} from 'semantic-ui-react'
 import './App.css';
 
 const {ipcRenderer} = window.require('electron');
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload..
-        </p>
-      </div>
+      <Grid className="main_container">
+
+        <Grid.Row className="entry_area">
+          <Grid.Column textAlign="center">
+            <Container>
+              <Divider hidden />
+              <Input size="big" placeholder="Add key conbination" />
+              <Divider hidden/>
+              <Form>
+                <TextArea autoHeight placeholder="Enter Text to be saved" />
+              </Form>
+              <Divider hidden />
+              <Button icon="save" content="Save"/>
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+          
+        <Divider />
+
+        <Grid.Row className="entries_list_area">
+          <Grid.Column>
+            <Container>
+              <Table  padded celled striped>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell collapsing >Time Added</Table.HeaderCell>
+                    <Table.HeaderCell>Shortcut</Table.HeaderCell>
+                    <Table.HeaderCell>Text</Table.HeaderCell>
+                    <Table.HeaderCell collapsing />
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+
+                  <Table.Row>
+                    <Table.Cell content="potato 1" />
+                    <Table.Cell content="potato 1" />
+                    <Table.Cell content="potato 1" />
+                    <Table.Cell><Button icon='delete'  circular /></Table.Cell>
+                  </Table.Row>
+
+                  <Table.Row>
+                     <Table.Cell content="potato 2" />
+                     <Table.Cell content="potato 2" />
+                     <Table.Cell content="potato 2" />
+                     <Table.Cell><Button icon='delete' circular /></Table.Cell>
+                  </Table.Row>
+
+                </Table.Body>
+                
+                <Table.Footer>
+                  <Table.Row>
+                     <Table.HeaderCell colSpan="4">
+                        <Button floated='right' icon labelPosition='left' primary negative size='small'>
+                          <Icon name='trash outline' /> Remove all
+                        </Button>
+                     </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Footer>
+          
+              </Table>
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+
+      </Grid>
     );
   }
 }
